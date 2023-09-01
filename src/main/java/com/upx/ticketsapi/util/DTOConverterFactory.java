d829dc42fbfa9dc5ac0011ceb1489a54cebb727a
Matheus Alexander <matheusalexalcantara@gmail.com>
2023-09-01 20:06:18 -0300

    feat: add abstract method to add a pattern to dto convertion

--- src/main/java/com/upx/ticketsapi/util/DTOConverterFactory.java
+++ src/main/java/com/upx/ticketsapi/util/DTOConverterFactory.java
@@ -1,15 +1,26 @@
 package com.upx.ticketsapi.util;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 import org.springframework.beans.BeanUtils;
 
+import com.upx.ticketsapi.exception.BusinessException;
+
 public class DTOConverterFactory {
+
     private DTOConverterFactory() {
         throw new IllegalAccessError("utility class");
     }
 
-    public static <T> T fromDTO(T dto, T toConvert){
-        BeanUtils.copyProperties(dto, toConvert);
-        
-        return toConvert;
+    public static <D,E> E fromDTO(D dto, Class<E> entityClass){
+        try {
+            Constructor<E> constructor = entityClass.getDeclaredConstructor();
+            E entity = constructor.newInstance();
+            BeanUtils.copyProperties(dto, entity);
+            return entity;
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            throw new BusinessException("Error creating entity instance");
+        }
     }
 }
