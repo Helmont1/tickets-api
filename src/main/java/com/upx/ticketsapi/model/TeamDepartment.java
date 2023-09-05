555d6aa62b360c998e93e6cb4392866f7a9eadd3
Matheus Alexander <matheusalexalcantara@gmail.com>
2023-09-05 18:53:34 -0300

    fix: add generatedValue to teamDepartmentId

--- src/main/java/com/upx/ticketsapi/model/TeamDepartment.java
+++ src/main/java/com/upx/ticketsapi/model/TeamDepartment.java
@@ -2,6 +2,8 @@ package com.upx.ticketsapi.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -17,6 +19,7 @@ import lombok.NoArgsConstructor;
 @Table(name = "team_department")
 public class TeamDepartment {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "team_department_id")
     private Integer teamDepartmentId;
 
