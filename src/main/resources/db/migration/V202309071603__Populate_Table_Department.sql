24dbaeb678748684079f7a2f1a8fa6176b554cba
Matheus Alexander <matheusalexalcantara@gmail.com>
2023-09-09 16:02:51 -0300

    feat: adds migration to populate database with real departments

---
+++ src/main/resources/db/migration/V202309071603__Populate_Table_Department.sql
@@ -0,0 +1,2 @@
+INSERT INTO Department (department_name, active)
+VALUES ('Pedagógico', true),('Seleção', true),('Social', true),('Pedagógico', true),('Departamento Pessoal', true),('Comercial', true);
