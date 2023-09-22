b890f424ef7e07f7dca422577431ca41989682f2
Matheus Alexander <matheusalexalcantara@gmail.com>
2023-09-22 01:30:26 -0300

    feat: add migration to populate table user with test data

---
+++ src/main/resources/db/migration/V202309220117__Populate_Table_User.sql
@@ -0,0 +1,47 @@
+INSERT INTO
+    Users (
+        user_name,
+        email,
+        cpf,
+        phone_number,
+        birth_date,
+        cep,
+        active
+    )
+VALUES
+    (
+        'Admin',
+        'admin@admin.com',
+        '12345678901',
+        '12345678901',
+        '1990-01-01',
+        '12345678',
+        TRUE
+    ),
+    (
+        'Jean Lucca',
+        'jean@email.com',
+        '12345678901',
+        '12345678901',
+        '1990-01-01',
+        '12345678',
+        TRUE
+    ),
+    (
+        'Rafael Vieira',
+        'rafael@email.com',
+        '12345678901',
+        '12345678901',
+        '1990-01-01',
+        '12345678',
+        TRUE
+    ),
+    (
+        'Whindersson',
+        'whindersson@email.com',
+        '12345678901',
+        '12345678901',
+        '1990-01-01',
+        '12345678',
+        TRUE
+    );
