a08f7b3c7e22e9dde9bf25bb7447797629427d1e
Matheus Alexander <matheusalexalcantara@gmail.com>
2023-09-22 19:37:19 -0300

    feat: create migration to populate team entity

---
+++ src/main/resources/db/migration/V202309221910__Populate_Table_Team.sql
@@ -0,0 +1,7 @@
+INSERT INTO
+    TEAM (leader, active, team_name)
+VALUES
+    (3, true, 'Time Rafael '),
+    (3, true, 'Time do Rafael - 2'),
+    (3, true, 'Time do Renan'),
+    (3, true, 'Time Renan - 2');
