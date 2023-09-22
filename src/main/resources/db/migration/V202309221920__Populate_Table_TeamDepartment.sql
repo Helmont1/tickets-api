a0190e2eb17f3834ea2b80b70ae58a411caf0d6b
Matheus Alexander <matheusalexalcantara@gmail.com>
2023-09-22 19:37:36 -0300

    feat: create migration to populate TeamDepartment entity

---
+++ src/main/resources/db/migration/V202309221920__Populate_Table_TeamDepartment.sql
@@ -0,0 +1,14 @@
+INSERT INTO
+    Team_Department (team_id, department_id, active)
+    VALUES 
+    (1,1,true),
+    (1,2,true),
+    (1,3,true),
+    (1,4,true),
+    (1,5,true),
+    (2,6,true),
+    (3,1,true),
+    (4,2,true),
+    (4,3,true),
+    (4,4,true),
+    (4,5,true);
