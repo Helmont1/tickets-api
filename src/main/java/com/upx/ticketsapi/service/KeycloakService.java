a356897186886e46e8ac8f772869403bf695b98a
Matheus Alexander <matheusalexalcantara@gmail.com>
2023-10-07 01:41:48 -0300

    feat: adds keycloakService with methods to get analysts and requesters from keycloak

---
+++ src/main/java/com/upx/ticketsapi/service/KeycloakService.java
@@ -0,0 +1,31 @@
+package com.upx.ticketsapi.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.representations.idm.UserRepresentation;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KeycloakService {
+    private final Keycloak keycloak;
+
+    public KeycloakService(Keycloak keycloak) {
+        this.keycloak = keycloak;
+    }
+
+    public List<UserRepresentation> getAnalystsInRealm() {
+        return keycloak.realm("upx-tickets").users()
+                .list().stream()
+                .filter(user -> user.getRealmRoles().contains("analyst"))
+                .collect(Collectors.toList());
+    }
+
+    public List<UserRepresentation> getRequestersInRealm() {
+        return keycloak.realm("upx-tickets").users()
+                .list().stream()
+                .filter(user -> user.getRealmRoles().contains("requester"))
+                .collect(Collectors.toList());
+    }
+}
