f9d1ab3b391de67e4cbe4f951520aaa7650ece41
Matheus Alexander <matheusalexalcantara@gmail.com>
2023-10-07 14:30:47 -0300

    fix: uses userResource to get roles and apply filter correctly

--- src/main/java/com/upx/ticketsapi/service/KeycloakService.java
+++ src/main/java/com/upx/ticketsapi/service/KeycloakService.java
@@ -1,10 +1,11 @@
 package com.upx.ticketsapi.service;
 
-import java.lang.reflect.Array;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.keycloak.admin.client.Keycloak;
+import org.keycloak.representations.idm.GroupRepresentation;
+import org.keycloak.representations.idm.RoleRepresentation;
 import org.keycloak.representations.idm.UserRepresentation;
 import org.springframework.stereotype.Service;
 
@@ -16,18 +17,22 @@ public class KeycloakService {
         this.keycloak = keycloak;
     }
 
-    public List<UserRepresentation> getAnalystsInRealm() {
-        return keycloak.realm("upx-tickets").users()
+    public List<UserRepresentation> getByRole(String role) {
+
+        var usersList = keycloak.realm("upx-tickets").users()
                 .list()
                 .stream()
-                .filter(user -> user.getRealmRoles() != null && user.getRealmRoles().contains("analyst"))
                 .collect(Collectors.toList());
-    }
 
-    public List<UserRepresentation> getRequestersInRealm() {
-        return keycloak.realm("upx-tickets").users()
-                .list().stream()
-                .filter(user -> user.getRealmRoles() != null ? user.getRealmRoles().contains("requester") : false)
+        for (var userRepresentation : usersList) {
+            var userResource = keycloak.realm("upx-tickets")
+                    .users().get(userRepresentation.getId());
+            userRepresentation.setRealmRoles(userResource.roles().getAll().getRealmMappings().stream().map(RoleRepresentation::getName).collect(Collectors.toList()));
+            userRepresentation.setGroups(userResource.groups().stream().map(GroupRepresentation::getName).collect(Collectors.toList()));
+        }
+
+        return usersList.stream()
+                .filter(user -> user.getRealmRoles() != null && user.getRealmRoles().contains(role))
                 .collect(Collectors.toList());
     }
 }
