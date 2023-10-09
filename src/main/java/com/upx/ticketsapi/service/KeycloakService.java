27f52f2c06d6584ae946b038ac8c339fe419927b
Matheus Alexander <matheusalexalcantara@gmail.com>
2023-10-09 20:03:57 -0300

    refactor: uses realm dynamically by keycloak property

--- src/main/java/com/upx/ticketsapi/service/KeycloakService.java
+++ src/main/java/com/upx/ticketsapi/service/KeycloakService.java
@@ -7,25 +7,28 @@ import org.keycloak.admin.client.Keycloak;
 import org.keycloak.representations.idm.GroupRepresentation;
 import org.keycloak.representations.idm.RoleRepresentation;
 import org.keycloak.representations.idm.UserRepresentation;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KeycloakService {
     private final Keycloak keycloak;
+    private final String realm;
 
-    public KeycloakService(Keycloak keycloak) {
+    public KeycloakService(Keycloak keycloak, Environment env) {
         this.keycloak = keycloak;
+        this.realm = env.getProperty("keycloak.realm");
     }
 
     public List<UserRepresentation> getByRole(String role) {
 
-        var usersList = keycloak.realm("upx-tickets").users()
+        var usersList = keycloak.realm(realm).users()
                 .list()
                 .stream()
                 .collect(Collectors.toList());
 
         for (var userRepresentation : usersList) {
-            var userResource = keycloak.realm("upx-tickets")
+            var userResource = keycloak.realm(realm)
                     .users().get(userRepresentation.getId());
             userRepresentation.setRealmRoles(userResource.roles().getAll().getRealmMappings().stream().map(RoleRepresentation::getName).collect(Collectors.toList()));
             userRepresentation.setGroups(userResource.groups().stream().map(GroupRepresentation::getName).collect(Collectors.toList()));
@@ -35,4 +38,5 @@ public class KeycloakService {
                 .filter(user -> user.getRealmRoles() != null && user.getRealmRoles().contains(role))
                 .collect(Collectors.toList());
     }
+
 }
